7052c6163192d54e99481fbfb17d037aef828949
ivan-zlatev <ivanzlatev90@gmail.com>
2018-02-09 17:22:58 +0200

    removed some duplicate files

--- private_data.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-LoginCredentials = {
-	'mysql_host' : "localhost",
-	'mysql_username' : "root",
-	'mysql_password' : "1234",
-	'mysql_db' : "testDB",
-	'mysql_table' : "testTable"
-}
