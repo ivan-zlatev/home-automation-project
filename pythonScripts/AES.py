ed6e34816b2f9a221ea530d6f1e673a988b185d6
ivan-zlatev <ivanzlatev90@gmail.com>
2018-02-12 14:33:44 +0200

    removed parent address from message encryption process

--- pythonScripts/AES.py
+++ pythonScripts/AES.py
@@ -12,23 +12,22 @@ def decryptMessage( message ):
 		return False
 	try:
 		decodedMessage = '{}'.format(cipher.decrypt(message))
-		return [ decodedMessage[8:10], decodedMessage[10:12], decodedMessage[12:] ]
+		return [ int(decodedMessage[10:12]), int(decodedMessage[12:]) ]
 	except:
 		print "Could not decrypt message"
 		return False
 
-def encryptMessage( dev_addr, sensor_addr, sensor_data ):
+def encryptMessage( sensor_addr, sensor_data ):
 	try:
 		cipher	= AES.new(LoginCredentials['AES_SECRET'], AES.MODE_CBC, LoginCredentials['AES_IV'])
 	except:
 		print "Could not initialize AES cipher"
 		return False
 	try:
-		dev_addr	= '{:0>2}'.format(str(dev_addr).lower())[0:2]
 		sensor_addr	= '{:0>2}'.format(str(sensor_addr).lower())[0:2]
 		sensor_data	= '{:0>4}'.format(str(sensor_data).lower())[0:4]
-		PRNG		= binascii.hexlify(os.urandom(4))
-		message		= PRNG + dev_addr + sensor_addr + sensor_data
+		PRNG		= binascii.hexlify(os.urandom(5))
+		message		= PRNG + sensor_addr + sensor_data
 		message		= message.zfill(16)
 	except:
 		print "Could not create message"
